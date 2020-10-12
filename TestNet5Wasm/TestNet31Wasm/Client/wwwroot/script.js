9c712314378e38102f7156501856ec782f468094
Stamo-Gochev <stamo_gochev@yahoo.com>
2020-10-12 14:14:28 +0300

    feat: add 3.1 wasm app

---
+++ TestNet5Wasm/TestNet31Wasm/Client/wwwroot/script.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    window.invokeFocusOut = function() {
+        DotNet.invokeMethodAsync("TestNet31Wasm.Client", "Custom_FocusOut");
+    }
+
+    window.document.addEventListener("focusout", function (e) {
+        console.log("focus out: " + new Date());
+
+        if (e.target.classList.contains("list-item")) {
+            invokeFocusOut();
+        }
+    });
+
+})();
