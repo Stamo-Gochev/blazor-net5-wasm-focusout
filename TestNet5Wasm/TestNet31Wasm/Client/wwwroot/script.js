5338a52f0e6191ae4b9a92e3c1beeb64f051e2fe
Stamo-Gochev <stamo_gochev@yahoo.com>
2020-10-12 14:30:18 +0300

    refactor: net31 app

--- TestNet5Wasm/TestNet31Wasm/Client/wwwroot/script.js
+++ TestNet5Wasm/TestNet31Wasm/Client/wwwroot/script.js
@@ -6,7 +6,7 @@
     window.document.addEventListener("focusout", function (e) {
         console.log("focus out: " + new Date());
 
-        if (e.target.classList.contains("list-item")) {
+        if (e.target.classList.contains("list-item-1")) {
             invokeFocusOut();
         }
     });
