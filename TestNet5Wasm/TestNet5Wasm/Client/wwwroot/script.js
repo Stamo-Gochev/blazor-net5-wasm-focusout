65785a2c1588d631916ac2ab5d9d4fe9882046f1
Stamo-Gochev <stamo_gochev@yahoo.com>
2020-10-15 07:55:02 +0300

    feat: make project runable in .net50 rc2

--- TestNet5Wasm/TestNet5Wasm/Client/wwwroot/script.js
+++ TestNet5Wasm/TestNet5Wasm/Client/wwwroot/script.js
@@ -1,13 +1,14 @@
 ﻿(function () {
     window.invokeFocusOut = function() {
-        DotNet.invokeMethodAsync("TestNet5Wasm", "Custom_FocusOut");
+        DotNet.invokeMethodAsync("TestNet5Wasm.Client", "Custom_FocusOut");
     }
 
     window.document.addEventListener("focusout", function (e) {
         console.log("focus out: " + new Date());
 
         if (e.target.classList.contains("list-item-1")) {
-            invokeFocusOut();
+            //invokeFocusOut();
+            setTimeout(invokeFocusOut, 0);
         }
     });
 
